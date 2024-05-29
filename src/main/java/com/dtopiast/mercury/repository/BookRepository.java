bf5e77266552cb3f9994eccff96eb405f39af446
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-05-29 16:58:26 -0600

    Updated repository to support search by name and display top 10

--- src/main/java/com/dtopiast/mercury/repository/BookRepository.java
+++ src/main/java/com/dtopiast/mercury/repository/BookRepository.java
@@ -2,7 +2,14 @@ package com.dtopiast.mercury.repository;
 
 import com.dtopiast.mercury.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    @Query("SELECT b FROM Book b WHERE b.name = :bookName")
+    Optional<Book> getBookByName(String bookName);
+    @Query("SELECT b FROM Book b ORDER BY b.id DESC")
+    List<Book> getTop10Books();
 }
