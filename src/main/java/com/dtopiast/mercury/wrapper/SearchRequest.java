23f153fa1f11c8724c2ff9edbbcac0a65115ec92
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-05-29 16:59:10 -0600

    Updated request to support more one lenguaje

--- src/main/java/com/dtopiast/mercury/wrapper/SearchRequest.java
+++ src/main/java/com/dtopiast/mercury/wrapper/SearchRequest.java
@@ -2,9 +2,9 @@ package com.dtopiast.mercury.wrapper;
 
 import java.util.List;
 
-public record SearchRequest(String name,
-                            List<String> author,
-                            String lenguaje,
+public record SearchRequest(String tittle,
+                            List<String> authors,
+                            List<String> lenguajes,
                             int authorBirthday,
                             int authorDead,
                             Boolean orderBy)
