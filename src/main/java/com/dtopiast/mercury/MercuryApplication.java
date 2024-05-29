c7b0f09f531531e538ae2685544ac8fb134ac602
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-05-29 07:31:13 -0600

    Created blank project

---
+++ src/main/java/com/dtopiast/mercury/MercuryApplication.java
@@ -0,0 +1,13 @@
+package com.dtopiast.mercury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercuryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtopiast/mercury/MercuryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtopiast.mercury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercuryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
