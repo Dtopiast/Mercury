46ee363d4fd81f48fa5cac32b9c258bb943285be
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-05-29 22:39:46 -0600

    Updated dto s

--- src/main/java/com/dtopiast/mercury/dto/AuthorDto.java
+++ src/main/java/com/dtopiast/mercury/dto/AuthorDto.java
@@ -1,8 +1,72 @@
 package com.dtopiast.mercury.dto;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
-public record AuthorDto(String Name,
-                        ArrayList<BookDto> bookDto) {
+public final class AuthorDto {
+    private final String Name;
+    private final int birthYear;
+    private final int deathYear;
+    private ArrayList<BookDto> books;
 
+    public <bookDto> AuthorDto(String Name,
+                               int birthYear,
+                               int deathYear,
+                               ArrayList<BookDto>books) {
+        this.Name = Name;
+        this.birthYear = birthYear;
+        this.deathYear = deathYear;
+        this.books = books;
+    }
+    public <bookDto> AuthorDto(String Name,
+                               int birthYear,
+                               int deathYear) {
+        this.Name = Name;
+        this.birthYear = birthYear;
+        this.deathYear = deathYear;
+    }
+
+    public String Name() {
+        return Name;
+    }
+
+    public int birthYear() {
+        return birthYear;
+    }
+
+    public int deathYear() {
+        return deathYear;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (AuthorDto) obj;
+        return Objects.equals(this.Name, that.Name) &&
+                this.birthYear == that.birthYear &&
+                this.deathYear == that.deathYear;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Name, birthYear, deathYear);
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorDto[" +
+                "Name=" + Name + ", " +
+                "birthYear=" + birthYear + ", " +
+                "deathYear=" + deathYear + ']';
+    }
+
+
+    public ArrayList<BookDto> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<BookDto> books) {
+        this.books = books;
+    }
 }
--- src/main/java/com/dtopiast/mercury/dto/BookDto.java
+++ src/main/java/com/dtopiast/mercury/dto/BookDto.java
@@ -1,9 +1,101 @@
 package com.dtopiast.mercury.dto;
 import java.util.ArrayList;
-public record BookDto(String title,
-                      ArrayList<AuthorDto> authors,
-                      ArrayList<String> subjects,
-                      ArrayList<String> bookshelves,
-                      ArrayList<String> languages,
-                      ArrayList<String> formats,
-                      boolean copyright) {}
+import java.util.Objects;
+
+public final class BookDto {
+    private final String title;
+    private ArrayList<AuthorDto> authors;
+    private final ArrayList<String> subjects;
+    private final ArrayList<String> bookshelves;
+    private final ArrayList<String> languages;
+    private final ArrayList<String> formats;
+    private final boolean copyright;
+
+    public BookDto(String title,
+                   ArrayList<AuthorDto> authors,
+                   ArrayList<String> subjects,
+                   ArrayList<String> bookshelves,
+                   ArrayList<String> languages,
+                   ArrayList<String> formats,
+                   boolean copyright) {
+        this.title = title;
+        this.authors = authors;
+        this.subjects = subjects;
+        this.bookshelves = bookshelves;
+        this.languages = languages;
+        this.formats = formats;
+        this.copyright = copyright;
+    }
+    public BookDto(String title,
+                   ArrayList<String> subjects,
+                   ArrayList<String> bookshelves,
+                   ArrayList<String> languages,
+                   ArrayList<String> formats,
+                   boolean copyright) {
+        this.title = title;
+        this.subjects = subjects;
+        this.bookshelves = bookshelves;
+        this.languages = languages;
+        this.formats = formats;
+        this.copyright = copyright;
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public ArrayList<AuthorDto> authors() {
+        return authors;
+    }
+
+    public ArrayList<String> subjects() {
+        return subjects;
+    }
+
+    public ArrayList<String> bookshelves() {
+        return bookshelves;
+    }
+
+    public ArrayList<String> languages() {
+        return languages;
+    }
+
+    public ArrayList<String> formats() {
+        return formats;
+    }
+
+    public boolean copyright() {
+        return copyright;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (BookDto) obj;
+        return Objects.equals(this.title, that.title) &&
+                Objects.equals(this.authors, that.authors) &&
+                Objects.equals(this.subjects, that.subjects) &&
+                Objects.equals(this.bookshelves, that.bookshelves) &&
+                Objects.equals(this.languages, that.languages) &&
+                Objects.equals(this.formats, that.formats) &&
+                this.copyright == that.copyright;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, authors, subjects, bookshelves, languages, formats, copyright);
+    }
+
+    @Override
+    public String toString() {
+        return "BookDto[" +
+                "title=" + title + ", " +
+                "authors=" + authors + ", " +
+                "subjects=" + subjects + ", " +
+                "bookshelves=" + bookshelves + ", " +
+                "languages=" + languages + ", " +
+                "formats=" + formats + ", " +
+                "copyright=" + copyright + ']';
+    }
+}
