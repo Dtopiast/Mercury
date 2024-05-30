63a57295c1fb0631fbadf06058c530128c6121f5
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-05-29 22:26:38 -0600

    Delete translators property in BookDto

--- src/main/java/com/dtopiast/mercury/dto/BookDto.java
+++ src/main/java/com/dtopiast/mercury/dto/BookDto.java
@@ -2,7 +2,6 @@ package com.dtopiast.mercury.dto;
 import java.util.ArrayList;
 public record BookDto(String title,
                       ArrayList<AuthorDto> authors,
-                      ArrayList<String> translators,
                       ArrayList<String> subjects,
                       ArrayList<String> bookshelves,
                       ArrayList<String> languages,
