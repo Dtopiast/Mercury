6a5255ad9fcac3e4b62b324ddebae07acd0e535c
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-05-29 21:12:23 -0600

    Added interface to search books

---
+++ src/main/java/com/dtopiast/mercury/service/IBookSearch.java
@@ -0,0 +1,16 @@
+package com.dtopiast.mercury.service;
+
+import com.dtopiast.mercury.wrapper.SearchRequest;
+import com.dtopiast.mercury.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IBookSearch {
+    List<Book> getAllBooks();
+    List<Book> getBooks(int quantity);
+    List<Book> queryBooks(SearchRequest dto);
+    List<Book> getTop10Books();
+    Optional<Book> getBookByName(String bookName);
+
+}
