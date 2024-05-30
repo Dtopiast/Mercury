708b123ca3d60cb8dceebe1a95171ac620e25483
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-05-30 12:26:28 -0600

    Added command line runner

--- src/main/java/com/dtopiast/mercury/MercuryApplication.java
+++ src/main/java/com/dtopiast/mercury/MercuryApplication.java
@@ -1,13 +1,19 @@
 package com.dtopiast.mercury;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MercuryApplication {
+public class MercuryApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MercuryApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		Program runner = new Program();
+		runner.showMenu();
 	}
 
 }
